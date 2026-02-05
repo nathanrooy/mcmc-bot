da354640cbbc6d7ff665d6e0c1d6c59350546005
Nathan Rooy <rooyna@mail.uc.edu>
2026-02-05 11:46:31 -0500

    Update nostr.py

--- src/nostr.py
+++ src/nostr.py
@@ -1,6 +1,6 @@
 import ssl
 import time
-from os import environ
+from os import _exit, environ
 
 from nostr.event import Event, EventKind
 from nostr.relay_manager import RelayManager
@@ -13,15 +13,23 @@ def post(msg):
     for relay in ["relay.damus.io", "nos.lol", "nostr.wine"]:
         relay_manager.add_relay(f"wss://{relay}")
     relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE})
-    time.sleep(2)
+    time.sleep(5)
 
-    private_key = PrivateKey.from_nsec(environ.get("NOSTR_PRIV"))
-    public_key = private_key.public_key
+    try:
+        private_key = PrivateKey.from_nsec(environ.get("NOSTR_PRIV"))
+        event = Event(private_key.public_key.hex(), msg, tags=[], kind=EventKind.TEXT_NOTE)
+        private_key.sign_event(event)
+        
+        print(f"> nostr :: publishing to relays...")
+        relay_manager.publish_event(event)
+        time.sleep(3)
 
-    event = Event(public_key.hex(), msg, tags=[], kind=EventKind.TEXT_NOTE)
+    except Exception as e:
+        print(f"> nostr :: post likely sent, but encountered an error: {e}")
 
-    private_key.sign_event(event)
-    relay_manager.publish_event(event)
-    time.sleep(1)
+    finally:
+        print("> nostr :: closing connections...")
+        relay_manager.close_connections()
 
-    relay_manager.close_connections()
+        # force exit to avoid zombie threads
+        _exit(0)
